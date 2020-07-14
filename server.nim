24802179e666a4837802d273a67ed66aa3754a9b
Jason Beetham <beefers331@gmail.com>
2020-07-14 16:07:58 -0600

    first commit

---
+++ server.nim
@@ -0,0 +1,12 @@
+import netty
+
+# listen for a connection on localhost port 1999
+var server = newReactor("127.0.0.1", 1999)
+# main loop
+while true:
+  server.tick()
+  #We be relaying
+  for msg in server.messages:
+    for client in server.connections:
+      if(msg.conn != client):
+        server.send(client, msg.data)
