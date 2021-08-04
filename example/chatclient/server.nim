9168461500f3c38f917a992f67d856e824bcb4dd
py-am-i <Wykleph@users.noreply.github.com>
2021-08-04 13:35:30 -0700

    Display message data from clients.

--- example/chatclient/server.nim
+++ example/chatclient/server.nim
@@ -9,13 +9,15 @@ nettyrpc.reactor = server
 # main loop
 while true:
   server.rpcTick(server=true)  
+  for msg in server.messages:  # Display messages since last tick.
+    echo msg
+    
   # rpcTick(server=true) will automatically loop through messages
   # and dispatch/relay RPCs.  It's the equivelant to the following
   # relay code:
   #
   # for msg in server.messages:
-  #   echo msg
   #   for client in server.connections:
   #     if(msg.conn != client):
   #       server.send(client, msg.data)
-  sleep(30)
+  # sleep(30)
